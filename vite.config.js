d6b5e590b0104ad6caa1c08ff86cb2b055002c83
Bettina Steger <hello@bettysteger.com>
2025-03-15 15:56:21 +0100

    add service worker

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,47 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,gif}'], // Cache static files
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/api\.tvmaze\.com\/.*$/,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'api-cache',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 5 * 60,
+              },
+              networkTimeoutSeconds: 10,
+            },
+          },
+          {
+            urlPattern: ({ request }) => request.destination === 'image',
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'image-cache',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 24 * 60 * 60,
+              },
+            },
+          },
+        ],
+        navigateFallback: '/index.html',
+      },
+      devOptions: {
+        enabled: true,
+      }
+    }),
   ],
   resolve: {
     alias: {
